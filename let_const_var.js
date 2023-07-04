1ce378bcfad48bc02a4b9b52b76554a1b64a0be8
Shubhamtest96 <subhamtest96@gmail.com>
2023-07-05 00:04:18 +0530

    code on let const var

---
+++ let_const_var.js
@@ -0,0 +1,26 @@
+
+  /// var is accesible outside its block or scope & u can re assign the value 
+// function myvar (){
+//        var a = 10 ; 
+//        if (true){
+//         var a = 20 ; 
+
+//         console.log(a)
+//        }
+//        console.log(a)
+// }
+
+// myvar();
+
+   // let is not acccesible outside of its scope 
+function mylet (){
+    let a = 70 ; 
+    if (true){
+     let a = 20 ; 
+
+     console.log(a)
+    }
+    console.log(a)
+}
+
+mylet();
